db9232889c4eac93d55a3226fe6a2c0f1906c473
Angela Yu <109006994+angelayuxh@users.noreply.github.com>
2022-08-09 23:50:34 -0400

    Create global.h

---
+++ include/global.h
@@ -0,0 +1,63 @@
+//1
+extern const int title[59][206];	
+//2
+extern const int lose[67][209];
+//3
+extern const int arrow[16][27] ;
+//4
+extern const int cross[23][23];	
+//5
+extern const int easy[36][96];	
+//6
+extern const int expert[36][96]; 	
+//7
+extern const int medium[36][104];	
+//8
+extern const int ezpress[31][96];
+//9
+extern const int exppress[31][96];	
+//10
+extern const int medpress[31][104];	
+//11
+extern const int help[36][96];	
+//12
+extern const int helppress[31][96];	
+//13
+extern const int newgame[36][96];	
+//14
+extern const int ngpress[31][96];
+//15
+extern const int check_button[36][52] ;	
+//16
+extern const int rule[149][259];
+	
+
+extern const int firework1[152][210];
+extern const int firework2[152][210];
+extern const int firework3[152][210];
+extern const int firework4[152][210];
+extern const int firework5[152][210];
+extern const int firework6[152][210];
+extern const int firework7[152][210];
+extern const int firework8[152][210];
+	
+
+extern const int n1[20][20];
+extern const int n2[20][20];
+extern const int n3[20][20];
+extern const int n4[20][20];
+extern const int n5[20][20];
+extern const int n6[20][20];
+extern const int n7[20][20];
+extern const int n8[20][20];
+extern const int n9[20][20];
+
+extern const int nn1[20][20];
+extern const int nn2[20][20];
+extern const int nn3[20][20];
+extern const int nn4[20][20];
+extern const int nn5[20][20];
+extern const int nn6[20][20];
+extern const int nn7[20][20];
+extern const int nn8[20][20];
+extern const int nn9[20][20];
